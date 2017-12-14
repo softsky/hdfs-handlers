303c89831201fbe1c91fe7ea5e2d79dde04579e9
amedvedchuk <amedvedchuk@jbs.com.ua>
2017-12-14 11:41:39 +0200

    Add HDFS file watcher stubs

---
+++ src/main/java/com/localblox/ashfaq/action/NewFileOutFolderActionAWSImpl.java
@@ -0,0 +1,15 @@
+package com.localblox.ashfaq.action;
+
+/**
+ * Action for processing output files with AWS.
+ */
+public class NewFileOutFolderActionAWSImpl extends NewFileOutFolderAction {
+
+    @Override
+    public void doIt(final String event) {
+
+        //TODO implement processing logic
+
+    }
+
+}
