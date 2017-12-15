3c020548f2c7a5c1bf0e49a33f6ec3eab44827a4
amedvedchuk <amedvedchuk@jbs.com.ua>
2017-12-15 15:52:20 +0200

    add test CSV file and read files with header

--- src/main/java/com/localblox/ashfaq/action/NewFileInFolderActionAWSImpl.java
+++ src/main/java/com/localblox/ashfaq/action/NewFileInFolderActionAWSImpl.java
@@ -11,9 +11,12 @@ import com.amazonaws.services.machinelearning.model.GetBatchPredictionResult;
 import com.amazonaws.services.machinelearning.model.GetDataSourceRequest;
 import com.amazonaws.services.machinelearning.model.GetDataSourceResult;
 import com.amazonaws.services.machinelearning.model.S3DataSpec;
+import org.apache.spark.sql.AnalysisException;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.TimeUnit;
 
@@ -21,6 +24,8 @@ import java.util.concurrent.TimeUnit;
  * Action for processing input files with AWS.
  */
 public class NewFileInFolderActionAWSImpl extends NewFileInFolderAction {
+
+    private static final Logger log = LoggerFactory.getLogger(NewFileInFolderActionAWSImpl.class);
 
     private static final String STATUS_COMPLETE = "COMPLETED";
 
@@ -106,6 +111,7 @@ public class NewFileInFolderActionAWSImpl extends NewFileInFolderAction {
 
     private Dataset<Row> readFromHDFS(SparkSession spark, String inFile) {
         Dataset<Row> ds = spark.read()
+                               .option("header", true)
                                .csv(inFile)
                                .select("Address", "City Name", "State Code", "County Name", "Zip Code",
                                        "Contact Person Name", "Contact Person Position",
@@ -121,11 +127,26 @@ public class NewFileInFolderActionAWSImpl extends NewFileInFolderAction {
                                        "Foursquare Profile", "HQ_Followers", "HQ_Type",
                                        "HQ_Employees", "HQ_Name", "HQ_YearFounded",
                                        "HQ_Categories", "HQ_Specialties", "HQ_Revenue",
-                                       "HQ_Ticker", "HQ_Exchange", "HQ_Acquisitions",
+                                       "HQ_Ticker", /*"HQ_Exchange",*/ "HQ_Acquisitions",
                                        "HQ_GrowthScore", "HQ_EstMonthlyUniques",
                                        "HQ_EstInboundLinks", "HQ_TwitterFollowers",
                                        "HQ_FacebookLikes", "HQ_FacebookTalkingAbout",
                                        "HQ_LinkedInFollowerCount");
+
+        // TODO - remove beffore production!!! needed to explore data!
+        // spark.sql("SELECT * FROM global_temp.aidata").show()
+        // FIXME doesn wor yet: scala> spark.sql("SELECT * FROM global_temp.aidata").show()
+        // FIXME org.apache.spark.sql.AnalysisException: Table or view not found: `global_temp`.`aidata`
+        try {
+            String view = "aidata";
+            ds.createGlobalTempView(view);
+            log.info("TEMRORARY VIEW CREATED FOR DATA EXPLORING: {}", view);
+            System.out.println("now you can explore the data...");
+            System.in.read();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
         return ds;
     }
 }
