a3eaa03341629ae79965026303e699dcfea1985f
abonert <bonertalexandr@gmail.com>
2017-12-15 13:18:22 +0200

    added simple connector to S3 and refactored code

--- src/main/java/com/localblox/ashfaq/action/NewFileInFolderActionAWSImpl.java
+++ src/main/java/com/localblox/ashfaq/action/NewFileInFolderActionAWSImpl.java
@@ -11,16 +11,10 @@ import com.amazonaws.services.machinelearning.model.GetBatchPredictionResult;
 import com.amazonaws.services.machinelearning.model.GetDataSourceRequest;
 import com.amazonaws.services.machinelearning.model.GetDataSourceResult;
 import com.amazonaws.services.machinelearning.model.S3DataSpec;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3EncryptionClientBuilder;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SQLContext;
 import org.apache.spark.sql.SparkSession;
 
-import java.io.InputStream;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -30,74 +24,30 @@ public class NewFileInFolderActionAWSImpl extends NewFileInFolderAction {
 
     private static final String STATUS_COMPLETE = "COMPLETED";
 
-    // TODO split the logic into separate methods of the class
     @Override
     public void doIt(final String inFile) throws RuntimeException {
         // 1. Read file from HDFS IN folder
-        SparkSession spark = SparkSession
-                .builder()
-                .appName("Java Spark SQL basic example")
-                .config("spark.master", "local")
-                .getOrCreate();
-//        Dataset<Row> dataSet = spark.read().csv(inFile);
-        SQLContext sqlContext = new SQLContext(spark);
-        Dataset<Row> df = sqlContext.read().format("com.databricks.spark.csv")
-                        .option("header", "true") // Use first line of all files as header
-                        .option("inferSchema", "true") // Automatically infer data types
-                        .load(inFile);
-
-        Dataset<Row> selectedData = df.select("Address", "City Name", "State Code", "County Name",
-                        "Zip Code", "Contact Person Name", "Contact Person Position",
-                        "Gender Contact Person 2", "Employee Count", "Employee Range",
-                        "Annual Revenues", "Sales Range", "SIC Name / Category", "Category",
-                        "Full Category Set", "Latitude", "Longitude", "Physical Neighborhood",
-                        "Love Score", "Freshness Score", "Holistic Score", "Hours of Operation",
-                        "Reviews Scanned", "Good Reviews Scanned", "Average Review Rating",
-                        "Likes Count", "Social Media Profiles Count", "Facebook Profile",
-                        "Twitter Profile", "Foursquare Profile", "HQ_Followers", "HQ_Type",
-                        "HQ_Employees", "HQ_Name", "HQ_YearFounded", "HQ_Categories",
-                        "HQ_Specialties", "HQ_Revenue", "HQ_Ticker", "HQ_Exchange",
-                        "HQ_Acquisitions", "HQ_GrowthScore", "HQ_EstMonthlyUniques",
-                        "HQ_EstInboundLinks", "HQ_TwitterFollowers", "HQ_FacebookLikes",
-                        "HQ_FacebookTalkingAbout", "HQ_LinkedInFollowerCount");
-
+        SparkSession spark = getSparkSession();
+        Dataset<Row> selectedData = readFromHDFS(spark, inFile);
         // init amazon machine learning client
         AmazonMachineLearning amazonMLClient = AmazonMachineLearningClientBuilder.defaultClient();
-        // 2. Create DataSource (name from filename)
-        CreateDataSourceFromS3Request createDataSourceFromS3Request = new CreateDataSourceFromS3Request();
-        S3DataSpec dataSpec = new S3DataSpec();
-        dataSpec.withDataLocationS3("s3://eml-test-EXAMPLE/data.csv").withDataSchemaLocationS3(
-                        "s3://eml-test-EXAMPLE/data.csv.schema");
-        createDataSourceFromS3Request.withDataSourceId("exampleDataSourceId")
-                        .withDataSourceName(inFile).withDataSpec(dataSpec);
-        CreateDataSourceFromS3Result dataSourceFromS3 = amazonMLClient.createDataSourceFromS3(createDataSourceFromS3Request);
-        // check if created
-        GetDataSourceRequest getDataSourceRequest = new GetDataSourceRequest().withDataSourceId(dataSourceFromS3.getDataSourceId());
-        GetDataSourceResult dataSource;
-        do {
-            dataSource = amazonMLClient.getDataSource(getDataSourceRequest);
-            try {
-                TimeUnit.MILLISECONDS.sleep(100L);
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
-        } while (STATUS_COMPLETE.equals(dataSource.getStatus()));
-
         // 3. Load input file to AWS datasource
-        // TODO convert Spark's DataSet to inputStream
-        AmazonS3 s3client = AmazonS3EncryptionClientBuilder.defaultClient();
-        InputStream inputStream = null;
-        ObjectMetadata objectMetadata = null;
-        s3client.putObject(new PutObjectRequest("bucketName", "keyName", inputStream, objectMetadata));
-
+        loadDataToS3(selectedData);
+        // 2. Create DataSource (name from filename)
+        GetDataSourceResult dataSource = createDataSource(amazonMLClient);
         // 4. Cretae Batch predition with params (DataSource ID, ...)
+        GetBatchPredictionResult batchPredictionResult = createBatchPrediction(amazonMLClient, dataSource);
+        // TODO 6. Read Prediction result from S3
+        // TODO 7. write result into OUT folder in HDFS
+    }
+
+    private GetBatchPredictionResult createBatchPrediction(AmazonMachineLearning amazonMLClient, GetDataSourceResult dataSource) {
         CreateBatchPredictionRequest createBatchPredictionRequest = new CreateBatchPredictionRequest();
         createBatchPredictionRequest.withBatchPredictionId("EXAMPLE-bp-2014-09-12-15-14-04-156")
-                        .withBatchPredictionName("EXAMPLE")
-                        .withMLModelId("EXAMPLE-pr-2014-09-12-15-14-04-924")
-                        .withBatchPredictionDataSourceId("EXAMPLE-tr-ds-2014-09-12-15-14-04-989")
-                        .withOutputUri("s3://eml-test-EXAMPLE/test-outputs/EXAMPLE-bp-2014-09-12-15-14-04-156/results");
-        // execute request
+                .withBatchPredictionName("EXAMPLE")
+                .withMLModelId("EXAMPLE-pr-2014-09-12-15-14-04-924")
+                .withBatchPredictionDataSourceId(dataSource.getDataSourceId())
+                .withOutputUri("s3://eml-test-EXAMPLE/test-outputs/EXAMPLE-bp-2014-09-12-15-14-04-156/results");
         CreateBatchPredictionResult createBatchPredictionResult = amazonMLClient.createBatchPrediction(createBatchPredictionRequest);
         String batchPredictionId = createBatchPredictionResult.getBatchPredictionId();
         // 5. Poll predtion status untill COMPLETE
@@ -112,9 +62,65 @@ public class NewFileInFolderActionAWSImpl extends NewFileInFolderAction {
                 Thread.currentThread().interrupt();
             }
         } while (!STATUS_COMPLETE.equals(batchPredictionResult.getStatus()));
-
-        // TODO 6. Read Prediction result from S3
-        // TODO 7. write result into OUT folder in HDFS
+        return batchPredictionResult;
     }
 
+    private void loadDataToS3(Dataset<Row> selectedData) {
+        selectedData.write().format("com.knoldus.spark.s3").option("accessKey", "s3_access_key")
+                        .option("secretKey", "s3_secret_key").option("bucket", "bucket_name")
+                        .option("fileType", "csv").save();
+    }
+
+    private GetDataSourceResult createDataSource(AmazonMachineLearning amazonMLClient) {
+        CreateDataSourceFromS3Request createDataSourceFromS3Request = new CreateDataSourceFromS3Request();
+        S3DataSpec dataSpec = new S3DataSpec();
+        dataSpec.withDataLocationS3("s3://eml-test-EXAMPLE/data.csv").withDataSchemaLocationS3(
+                        "s3://eml-test-EXAMPLE/data.csv.schema");
+        createDataSourceFromS3Request.withDataSourceId("exampleDataSourceId")
+                        .withDataSourceName("exampleDataSourceName").withDataSpec(dataSpec);
+        CreateDataSourceFromS3Result dataSourceFromS3 = amazonMLClient
+                        .createDataSourceFromS3(createDataSourceFromS3Request);
+        // check if created
+        GetDataSourceRequest getDataSourceRequest = new GetDataSourceRequest()
+                        .withDataSourceId(dataSourceFromS3.getDataSourceId());
+        GetDataSourceResult dataSource;
+        do {
+            dataSource = amazonMLClient.getDataSource(getDataSourceRequest);
+            try {
+                TimeUnit.MILLISECONDS.sleep(100L);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        } while (STATUS_COMPLETE.equals(dataSource.getStatus()));
+        return dataSource;
+    }
+
+    private SparkSession getSparkSession() {
+        return SparkSession.builder().appName("Java Spark SQL basic example")
+                        .config("spark.master", "local").getOrCreate();
+    }
+
+    private Dataset<Row> readFromHDFS(SparkSession spark, String inFile) {
+        return spark.read()
+                        .csv(inFile)
+                        .select("Address", "City Name", "State Code", "County Name", "Zip Code",
+                                        "Contact Person Name", "Contact Person Position",
+                                        "Gender Contact Person 2", "Employee Count",
+                                        "Employee Range", "Annual Revenues", "Sales Range",
+                                        "SIC Name / Category", "Category", "Full Category Set",
+                                        "Latitude", "Longitude", "Physical Neighborhood",
+                                        "Love Score", "Freshness Score", "Holistic Score",
+                                        "Hours of Operation", "Reviews Scanned",
+                                        "Good Reviews Scanned", "Average Review Rating",
+                                        "Likes Count", "Social Media Profiles Count",
+                                        "Facebook Profile", "Twitter Profile",
+                                        "Foursquare Profile", "HQ_Followers", "HQ_Type",
+                                        "HQ_Employees", "HQ_Name", "HQ_YearFounded",
+                                        "HQ_Categories", "HQ_Specialties", "HQ_Revenue",
+                                        "HQ_Ticker", "HQ_Exchange", "HQ_Acquisitions",
+                                        "HQ_GrowthScore", "HQ_EstMonthlyUniques",
+                                        "HQ_EstInboundLinks", "HQ_TwitterFollowers",
+                                        "HQ_FacebookLikes", "HQ_FacebookTalkingAbout",
+                                        "HQ_LinkedInFollowerCount");
+    }
 }
